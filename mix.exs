2e35cc114ebfd38765d7dfed59cc151783b169c1
wade <xingxing@xingtuwuxian.com>
2016-09-22 18:15:08 +0800

    chore: add Retrieval to deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule BoggleBoard.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {BoggleBoard, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +28,6 @@ defmodule BoggleBoard.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:retrieval, "~> 0.9.1"}]
   end
 end
